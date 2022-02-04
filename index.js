189c1118329810e9b60a9bbbe36609a32b40a7a8
Al Shahadath <shahadath@Als-MacBook-Air.local>
2022-02-04 19:37:05 +0600

    create io function to connect with frontend

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.get("/", (req, res) => {
   res.send("hello World");
 });
 
+io.on("connection", (socket) => {
+  console.log("Someone Connected");
+  socket.on("join-room", ({ roomId, userName }) => {
+    console.log("User Joined room");
+    console.log(roomId);
+    console.log(userName);
+  });
+});
+
 server.listen(port, () => {
   console.log(`Zoom Clone Api listening on localhost:3001`);
 });
